3a324c1cf32074f1ce7d9c5b311e9f0653f0ef35
Clark Brown <clark@clark-brown.com>
2024-12-14 20:09:24 -0700

    part 2 complete

--- src/1/__init__.py
+++ src/1/__init__.py
@@ -1,13 +1,14 @@
 from typing import TextIO
+from collections import defaultdict
 
 
 def part1(input: TextIO) -> int:
     l1 = list()
     l2 = list()
     for line in input:
-        s1, s2 = line.strip().split("   ")
-        l1.append(int(s1))
-        l2.append(int(s2))
+        left, right = line.strip().split("   ")
+        l1.append(int(left))
+        l2.append(int(right))
 
     l1.sort()
     l2.sort()
@@ -20,4 +21,17 @@ def part1(input: TextIO) -> int:
 
 
 def part2(input: TextIO) -> int:
-    raise NotImplementedError("day 1 part 2 not implemented")
+    l1 = defaultdict(int)
+    l2 = defaultdict(int)
+    for line in input:
+        left, right = line.strip().split("   ")
+        l1[left] += 1
+        l2[right] += 1
+
+    similarity_score = 0
+    for number, left_count in l1.items():
+        right_count = l2[number]
+        incremental_similarity = int(number) * left_count * right_count
+        similarity_score += incremental_similarity
+
+    return similarity_score
