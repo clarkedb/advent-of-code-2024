2172daab17bcf144a1fea1b648a061e6e3f38314
Clark Brown <clark@clark-brown.com>
2024-12-14 19:59:27 -0700

    part 1 working

--- src/1/__init__.py
+++ src/1/__init__.py
@@ -1,9 +1,23 @@
 from typing import TextIO
 
 
-def part1(input: TextIO) -> str:
-    raise NotImplementedError("day 1 part 1 not implemented")
+def part1(input: TextIO) -> int:
+    l1 = list()
+    l2 = list()
+    for line in input:
+        s1, s2 = line.strip().split("   ")
+        l1.append(int(s1))
+        l2.append(int(s2))
+
+    l1.sort()
+    l2.sort()
+
+    diff_sum = 0
+    for n1, n2 in zip(l1, l2):
+        diff_sum += abs(n1 - n2)
+
+    return diff_sum
 
 
-def part2(input: TextIO) -> str:
+def part2(input: TextIO) -> int:
     raise NotImplementedError("day 1 part 2 not implemented")
