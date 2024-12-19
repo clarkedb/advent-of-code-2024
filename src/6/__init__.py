9b89b587252999e6500277364ac98cd8e62b4b6e
Clark Brown <clark@clark-brown.com>
2024-12-18 20:57:49 -0700

    part 2 complete

--- src/6/__init__.py
+++ src/6/__init__.py
@@ -2,7 +2,6 @@ from typing import TextIO
 
 
 def part1(input: TextIO) -> int:
-    visited = set()
     map = list()
     guard_cell = None
     for i, line in enumerate(input):
@@ -22,6 +21,7 @@ def part1(input: TextIO) -> int:
     n = len(map)
     m = len(map[0])
     direction = (0, -1)
+    visited = set()
 
     while True:
         x, y = guard_cell
@@ -43,7 +43,58 @@ def part1(input: TextIO) -> int:
 
 
 def part2(input: TextIO) -> int:
-    raise NotImplementedError("day 6 part 2 not implemented")
+    map = list()
+    init_guard_cell = None
+    for i, line in enumerate(input):
+        row = list(line.strip())
+        map.append(row)
+        if init_guard_cell is not None:
+            continue
+        try:
+            j = row.index("^")
+            init_guard_cell = (j, i)
+        except ValueError:
+            continue
+
+    if init_guard_cell is None:
+        raise ValueError("no starting guard position detected")
+
+    n = len(map)
+    m = len(map[0])
+    loop_obstructions = set()
+
+    for i in range(n):
+        for j in range(m):
+            if map[i][j] != ".":
+                # ignore guard or existing blocks
+                continue
+
+            direction = (0, -1)
+            visited = set()
+            guard_cell = init_guard_cell
+
+            while True:
+                x, y = guard_cell
+
+                cell_state = (guard_cell, direction)
+                if cell_state in visited:
+                    loop_obstructions.add((j, i))
+                    break
+
+                visited.add(cell_state)
+
+                x_prime, y_prime = x + direction[0], y + direction[1]
+                if (x_prime < 0) or (y_prime < 0) or (x_prime >= m) or (y_prime >= n):
+                    break
+
+                if map[y_prime][x_prime] == "#" or (x_prime == j and y_prime == i):
+                    # road block, must turn
+                    direction = _turn_right(direction)
+                else:
+                    # advance
+                    guard_cell = (x_prime, y_prime)
+
+    return len(loop_obstructions)
 
 
 def _turn_right(direction: tuple[int, int]) -> tuple[int, int]:
