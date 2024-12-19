6c9adbd7a39775db8b698456179c5570d188f154
Clark Brown <clark@clark-brown.com>
2024-12-18 21:42:09 -0700

    part 2 complete

--- src/7/__init__.py
+++ src/7/__init__.py
@@ -10,31 +10,46 @@ def part1(input: TextIO) -> int:
         test_value = int(test_value_str)
         operands = list(map(int, operands_str.split(" ")))
 
-        if _can_calculate_target(test_value, operands):
+        if _can_calculate_target(test_value, operands, ["+", "*"]):
             total += test_value
 
     return total
 
 
 def part2(input: TextIO) -> int:
-    raise NotImplementedError("day 7 part 2 not implemented")
+    total = 0
+
+    for line in input:
+        test_value_str, operands_str = line.strip().split(": ")
+        test_value = int(test_value_str)
+        operands = list(map(int, operands_str.split(" ")))
+
+        if _can_calculate_target(test_value, operands, ["+", "*", "||"]):
+            total += test_value
+
+    return total
 
 
 def _evaluate_expression(operands, operators):
     result = operands[0]
 
     for i, op in enumerate(operators):
-        if op == "+":
-            result += operands[i + 1]
-        elif op == "*":
-            result *= operands[i + 1]
+        match op:
+            case "+":
+                result += operands[i + 1]
+            case "*":
+                result *= operands[i + 1]
+            case "||":
+                result = int(str(result) + str(operands[i + 1]))
+            case _:
+                raise ArithmeticError
 
     return result
 
 
-def _can_calculate_target(test_value, operands):
+def _can_calculate_target(test_value, operands, operators):
     n = len(operands) - 1
-    operator_combinations = product(["+", "*"], repeat=n)
+    operator_combinations = product(operators, repeat=n)
 
     for operators in operator_combinations:
         if _evaluate_expression(operands, operators) == test_value:
